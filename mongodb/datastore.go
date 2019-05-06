dabf82fab2c3ab8489994e1ac9f18f54675706d4
Ben Pate <ben@pate.org>
2019-05-06 15:35:23 -0600

    Documentation
    Adding godoc comments

--- mongodb/datastore.go
+++ mongodb/datastore.go
@@ -8,11 +8,15 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// Datastore is an abstract representation of a MongoDB database.  It implements the data.Datastore interface,
+// so that it should be usable anywhere that requires a data.Datastore.
 type Datastore struct {
 	uri      string
 	database string
 }
 
+// New returns a fully populated mongodb.Datastore.  It requires that you provide the URI for the mongodb
+// cluster, along with the name of the database to be used for all transactions.
 func New(uri string, database string) Datastore {
 
 	return Datastore{
@@ -21,6 +25,7 @@ func New(uri string, database string) Datastore {
 	}
 }
 
+// Session returns a new client session that can be used to perform CRUD transactions on this datastore.
 func (db Datastore) Session(ctx context.Context) data.Session {
 
 	client, err := mongo.NewClient(options.Client().ApplyURI(db.uri))
