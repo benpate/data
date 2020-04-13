83d4675b0edc7b161db5821287c3712232cd9a2a
Ben Pate <ben@pate.org>
2020-04-13 11:23:47 -0600

    Update datastore.go
    code comments

--- datastore.go
+++ datastore.go
@@ -1,3 +1,6 @@
+// Package data provides a data structure for defining simple database filters.  This
+// is not able to represent every imaginable query criteria, but it does a good job of making
+// common criteria simple to format and pass around in your application.
 package data
 
 import (
