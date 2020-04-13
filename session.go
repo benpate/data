178be29c10d401cf48587d9cf6c53cd336d6692a
Ben Pate <ben@pate.org>
2020-04-13 11:23:41 -0600

    Update session.go
    - using new library locations

--- session.go
+++ session.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"github.com/benpate/data/expression"
 	"github.com/benpate/data/option"
 	"github.com/benpate/derp"
 )
@@ -8,8 +9,8 @@ import (
 // Session represents a single database session, that is opened to support a single transactional request, and then closed
 // when this transaction is complete
 type Session interface {
-	List(collection string, filter Expression, options ...option.Option) (Iterator, *derp.Error)
-	Load(collection string, filter Expression, target Object) *derp.Error
+	List(collection string, filter expression.Expression, options ...option.Option) (Iterator, *derp.Error)
+	Load(collection string, filter expression.Expression, target Object) *derp.Error
 	Save(collection string, object Object, note string) *derp.Error
 	Delete(collection string, object Object, note string) *derp.Error
 	Close()
