96df3893eb17f85a12a22a26d22989f1c4e97f15
Ben Pate <ben@pate.org>
2020-07-23 11:54:15 -0600

    Exposing MongoDB database
    - in some (rare) cases, we need to expose the underlying MongoDB database outside of this package.

--- mongodb/server.go
+++ mongodb/server.go
@@ -12,8 +12,8 @@ import (
 // Server is an abstract representation of a MongoDB database.  It implements the data.Server interface,
 // so that it should be usable anywhere that requires a data.Server.
 type Server struct {
-	client   *mongo.Client
-	database *mongo.Database
+	Client   *mongo.Client
+	Database *mongo.Database
 }
 
 // New returns a fully populated mongodb.Server.  It requires that you provide the URI for the mongodb
@@ -27,8 +27,8 @@ func New(uri string, database string) (Server, *derp.Error) {
 	}
 
 	result := Server{
-		client:   client,
-		database: client.Database(database),
+		Client:   client,
+		Database: client.Database(database),
 	}
 
 	return result, nil
@@ -37,12 +37,12 @@ func New(uri string, database string) (Server, *derp.Error) {
 // Session returns a new client session that can be used to perform CRUD transactions on this datastore.
 func (db Server) Session(ctx context.Context) (data.Session, *derp.Error) {
 
-	if err := db.client.Connect(ctx); err != nil {
+	if err := db.Client.Connect(ctx); err != nil {
 		return nil, derp.New(500, "data.mongodb.Session", "Could not connect to client database", err)
 	}
 
 	result := Session{
-		database: db.database,
+		database: db.Database,
 		context:  ctx,
 	}
 
