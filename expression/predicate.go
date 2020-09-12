8d3ff506f2deeefe68abcf07ca7397c7a8ad687b
Ben Pate <ben@pate.org>
2020-09-11 19:18:01 -0600

    Adding new "create" functions

--- expression/predicate.go
+++ expression/predicate.go
@@ -16,6 +16,36 @@ func New(field string, operator string, value interface{}) Predicate {
 	}
 }
 
+// Equal creates a new Predicate using an "Equals" comparison
+func Equal(field string, value interface{}) Predicate {
+	return New(field, OperatorEqual, value)
+}
+
+// NotEqual creates a new Predicate using an "Not Equals" comparison
+func NotEqual(field string, value interface{}) Predicate {
+	return New(field, OperatorNotEqual, value)
+}
+
+// LessThan creates a new Predicate using an "Less Than" comparison
+func LessThan(field string, value interface{}) Predicate {
+	return New(field, OperatorLessThan, value)
+}
+
+// LessOrEqual creates a new Predicate using an "Less Or Equal" comparison
+func LessOrEqual(field string, value interface{}) Predicate {
+	return New(field, OperatorLessOrEqual, value)
+}
+
+// GreaterThan creates a new Predicate using an "Greater Than" comparison
+func GreaterThan(field string, value interface{}) Predicate {
+	return New(field, OperatorGreaterThan, value)
+}
+
+// GreaterOrEqual creates a new Predicate using an "Greater Or Equal" comparison
+func GreaterOrEqual(field string, value interface{}) Predicate {
+	return New(field, OperatorGreaterOrEqual, value)
+}
+
 // And combines this predicate with another one (created from the arguments) into an AndExpression
 func (predicate Predicate) And(field string, operator string, value interface{}) AndExpression {
 	return AndExpression{predicate, New(field, operator, value)}
