c46370eca48a5f7160752405e79aa9fade2da75b
Ben Pate <ben@pate.org>
2021-12-05 10:09:08 -0700

    Minor updates
    - switched to UnixNano() timestamps
    - fixed .HardDelete function definition

--- collection.go
+++ collection.go
@@ -8,9 +8,9 @@ import (
 // Collection represents a single database collection (or table) that is opened to support a single transactional request, and then closed
 // when this transaction is complete
 type Collection interface {
-	List(filter exp.Expression, options ...option.Option) (Iterator, error) // Query the collection
-	Load(filter exp.Expression, target Object) error                        // Load an object
-	Save(object Object, note string) error                                  // Insert/Update an object
-	Delete(object Object, note string) error                                // Virtual delete of an object
-	HardDelete(Object) error                                                // Physical delete of an object
+	List(filter exp.Expression, options ...option.Option) (Iterator, error)
+	Load(filter exp.Expression, target Object) error
+	Save(object Object, note string) error
+	Delete(object Object, note string) error
+	HardDelete(object Object) error
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/benpate/data
 
 go 1.16
 
-require (
-	github.com/benpate/exp v0.0.2
-	github.com/stretchr/testify v1.7.0
-)
+require github.com/benpate/exp v0.0.2
