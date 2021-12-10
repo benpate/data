d2ebe84eaf03f9911564e1841c466f3c274baf2e
Ben Pate <ben@pate.org>
2021-12-10 11:24:49 -0700

    Clarifying behavior of "FirstRow"

--- option/firstRow.go
+++ option/firstRow.go
@@ -4,19 +4,14 @@ package option
 const TypeFirstRow = "FIRSTROW"
 
 // FirstRowConfig is a query option that sets the first row to be returned in a dataset
-type FirstRowConfig int
+type FirstRowConfig struct{}
 
 // FirstRow returns a query option that will limit the query results to a certain number of rows
-func FirstRow(firstRow int64) Option {
-	return FirstRowConfig(firstRow)
+func FirstRow() Option {
+	return FirstRowConfig{}
 }
 
 // OptionType identifies this record as a query option
 func (firstRowConfig FirstRowConfig) OptionType() string {
 	return TypeFirstRow
-}
-
-// FirstRow returns the first row of the dataset to return
-func (firstRowConfig FirstRowConfig) FirstRow() int {
-	return int(firstRowConfig)
 }
