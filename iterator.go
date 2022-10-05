7b7a0230a09fd4c72f3a00860322c6abad8da9ee
Ben Pate <ben@pate.org>
2022-10-04 21:03:45 -0600

    Open up limits on Iterator types

--- iterator.go
+++ iterator.go
@@ -2,7 +2,7 @@ package data
 
 // Iterator interface allows callers to iterator over a large number of items in an array/slice
 type Iterator interface {
-	Next(Object) bool
+	Next(any) bool
 	Count() int
 	Close() error
 }
