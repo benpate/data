aae07c7bced6b6904efcdf47c7cbcbdc708f93a5
Ben Pate <ben@pate.org>
2022-10-28 12:07:53 -0600

    Add iterator.Error method

--- iterator.go
+++ iterator.go
@@ -3,6 +3,7 @@ package data
 // Iterator interface allows callers to iterator over a large number of items in an array/slice
 type Iterator interface {
 	Next(any) bool
+	Error() error
 	Count() int
 	Close() error
 }
