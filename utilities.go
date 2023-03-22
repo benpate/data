f49af5b4b38471cb02dfc6f0afbbaf4864a2a9f6
Ben Pate <ben@pate.org>
2023-03-21 22:32:23 -0600

    New utils for creating Channels

--- utilities.go
+++ utilities.go
@@ -15,3 +15,59 @@ func Slice[T any](iterator Iterator, constructor func() T) []T {
 
 	return result
 }
+
+// Channel converts an Iterator into a channel of items.
+// You must include a constructor function that generates fully initialized values of the type you want to return.
+func Channel[T any](iterator Iterator, constructor func() T) chan T {
+
+	if iterator.Count() == 0 {
+		return nil
+	}
+
+	result := make(chan T, 1) // Length of 1 to prevent blocking on the first item.
+
+	go func() {
+
+		value := constructor()
+
+		for iterator.Next(&value) {
+			result <- value
+			value = constructor()
+		}
+		close(result)
+	}()
+
+	return result
+}
+
+// Channel converts an Iterator into a channel of items.
+// You must include a constructor function that generates fully initialized values of the type you want to return.
+func ChannelWithCancel[T any](iterator Iterator, constructor func() T, cancel <-chan bool) chan T {
+
+	if iterator.Count() == 0 {
+		return nil
+	}
+
+	result := make(chan T, 1) // Length of 1 to prevent blocking on the first item.
+
+	go func() {
+		value := constructor()
+
+		for iterator.Next(&value) {
+			select {
+			case <-cancel:
+				close(result)
+				return
+
+			default:
+				result <- value
+			}
+
+			value = constructor()
+		}
+
+		close(result)
+	}()
+
+	return result
+}
