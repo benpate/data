a8be88fc83bece3686d1a88cefa6f9147cf6fcd0
Ben Pate <ben@pate.org>
2024-01-22 16:08:51 -0700

    Update dependencies

--- iterator.go
+++ iterator.go
@@ -1,6 +1,6 @@
 package data
 
-// Iterator interface allows callers to iterator over a large number of items in an array/slice
+// Iterator interface allows callers to iterator over a large number of items in a data structure
 type Iterator interface {
 	Next(any) bool
 	Error() error
