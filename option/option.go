53fc9fef16defbc3ccb790334e53a565bcfc0715
Ben Pate <ben@pate.org>
2026-06-13 12:16:44 -0600

    Comment

--- option/option.go
+++ option/option.go
@@ -5,5 +5,5 @@ package option
 // Option is an interface that wraps all of the query options that can be used to modify
 // a database query.
 type Option interface {
-	OptionType() string
+	OptionType() string // OptionType identifies the type of query option, and may be used for debugging purposes.
 }
